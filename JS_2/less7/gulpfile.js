065367c7c10bbf9df5b18acac378dd9fe14964e0
Unknown <admin@e-lime.ru>
2018-07-22 15:50:43 +0500

    ДЗ 7 NEW

--- JS_2/less7/gulpfile.js
+++ JS_2/less7/gulpfile.js
@@ -1,28 +1,30 @@
 const gulp = require('gulp');
-	  gulpif = require('gulp-if'), // Минификация JS
+	  gulpif = require('gulp-if'),
 	  useref = require('gulp-useref'), 
-	  uglify = require('gulp-uglify'), // Минификация JS
-      concat = require('gulp-concat'), // Склейка файлов
-      csso = require('gulp-csso'), // Минификация CSS
-      sass = require('gulp-sass'); // Конверстация SASS (SCSS) в CSS
+	  uglify = require('gulp-uglify'),
+      concat = require('gulp-concat'),
+      csso = require('gulp-csso'),
+      sass = require('gulp-sass'); 
 
-// Задача "sass". Запускается командой "gulp sass"
 gulp.task('sass', function () { 
- gulp.src('./dev/scss/style.scss') // файл, который обрабатываем
- .pipe(sass().on('error', sass.logError)) // конвертируем sass в css
- .pipe(csso()) // минифицируем css, полученный на предыдущем шаге
- .pipe(gulp.dest('./build/css/')); // результат пишем по указанному адресу
+ gulp.src([
+ 	'./dev/scss/style.scss',
+ 	'./dev/scss/style2.scss',
+ 	])
+ .pipe(concat('main.scss'))
+ .pipe(sass().on('error', sass.logError)) 
+ .pipe(csso())
+ .pipe(gulp.dest('./build/css/'));
 });
  
-// Задача "js". Запускается командой "gulp js"
 gulp.task('js', function() {
     gulp.src([
                 './dev/js/plagin.js',
                 './dev/js/script.js'
-        ]) // файлы, которые обрабатываем
-        .pipe(concat('main.js')) // склеиваем все JS
-        .pipe(uglify()) // получившуюся "портянку" минифицируем 
-        .pipe(gulp.dest('./build/js/')) // результат пишем по указанному адресу
+        ])
+        .pipe(concat('main.js'))
+        .pipe(uglify()) 
+        .pipe(gulp.dest('./build/js/')) 
 });
 
 gulp.task('build', function() {
